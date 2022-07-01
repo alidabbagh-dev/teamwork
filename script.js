df4911632440311e720c54b924386b8fa814fca5
mohammad <mohammad.masoud80@gmail.com>
2022-07-01 13:35:57 +0430

    script

---
+++ script.js
@@ -0,0 +1,22 @@
+const Obj=[
+
+    {
+        name:"ali",
+        family:"dabagh"
+    },
+
+]
+function Lists () { 
+    return [
+        {
+        name:"ali",
+        family:"dabagh"
+
+    },
+    {
+        name:"mohammad",
+        family:"masoud"
+
+    },
+]
+ }
